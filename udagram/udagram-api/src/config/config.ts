393c7c2d998ac83a8d63607be9733cd4b62f0d83
Samy Isaac <samyisaac7@gmail.com>
2022-12-18 22:10:39 +0200

    first commit

---
+++ udagram/udagram-api/src/config/config.ts
@@ -0,0 +1,23 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// ENV variables
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+// Are Also needed
+
+export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.PORT),
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
+  awsRegion: process.env.AWS_REGION,
+  awsProfile: process.env.AWS_PROFILE,
+  awsMediaBucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
